66819b431ca93ede0bc596bb7d9bc37bd916080c
Alex Bardanov <dnbard@gmail.com>
2014-12-29 23:28:28 +0200

    some improvements and error handling

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ var through = require('through2');
 var gm = require('gm');
 
 module.exports = function (options) {
-//    if (!options.foo) {
-//        throw new gutil.PluginError('gulp-hexsquare', '`foo` required');
-//    }
+    if (typeof options.size !== 'object') {
+        throw new gutil.PluginError('gulp-hexsquare', '`size` required');
+    }
+
+    if (typeof options.size.width !== 'number'){
+        throw new gutil.PluginError('gulp-hexsquare', '`size.width` must be a number');
+    }
+
+    if (typeof options.size.height !== 'number'){
+        throw new gutil.PluginError('gulp-hexsquare', '`size.height` must be a number');
+    }
 
     return through.obj(function (file, enc, cb) {
         if (file.isNull()) {
@@ -20,9 +28,15 @@ module.exports = function (options) {
         }
 
         try {
-            var gmFile = gm(file.contents, file.path);
+            var gmFile = gm(file.contents, file.path),
+                newSize = {
+                    width: Math.round(options.size.width / 1.5),
+                    height: Math.round(options.size.height / 1.5),
+                    x: Math.round(options.size.width / 6),
+                    y: Math.round(options.size.height / 6)
+                };
 
-            gmFile.crop(parseInt(options.size.width / 1.5), parseInt(options.size.height / 1.5), parseInt(options.size.width / 6), parseInt(options.size.height / 6));
+            gmFile.crop(newSize.width, newSize.height, newSize.x, newSize.y);
 
             if (options.resize){
                 gmFile.resize(options.resize.width, options.resize.height);
